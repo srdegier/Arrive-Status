5046d830124321614fca93f8834171758a0412f4
Stefan de Gier <stefan_de_gier@outlook.com>
2023-03-02 17:03:16 +0100

    Initial Commit

---
+++ Arrive Status/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Arrive Status
+//
+//  Created by Stefan de Gier on 02/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
