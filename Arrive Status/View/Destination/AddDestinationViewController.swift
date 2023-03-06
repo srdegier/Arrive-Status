e3d973d27e7ef270aadbf84f3647dcac8d326cdd
Stefan de Gier <stefan_de_gier@outlook.com>
2023-03-06 19:38:26 +0100

    Removed typo DestinationAddViewController

--- Arrive Status/View/Destination/AddDestinationViewController.swift
+++ Arrive Status/View/Destination/AddDestinationViewController.swift
@@ -12,7 +12,7 @@ class AddDestinationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.title = "Add Destinations"
+        self.title = "Add Destination"
     }
     
 }
